3f0aa7a54eb42c891371a37453998703771e1d4b
TylerTalbot <tt23370@mbc.school.nz>
2026-06-30 12:26:24 +1200

    Completed More General Code

--- AS 91896 Python Assessment.py
+++ AS 91896 Python Assessment.py
@@ -2,3 +2,10 @@ import random
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
+
+root = tk.Tk()
+root.geometry("400x400")
+root.grid_columnconfigure(2, weight=1)
+root.title("Party Hire Shop")
+root.configure(bg='orange')
+customers = []
